3519ee4e030691334b00badd56b9c3eba3c24058
Sajal <mahatosajal65@gmail.com>
2024-09-29 18:57:59 +0530

    * get largest & smallest number from an array

--- Arrays/ArrayCC.java
+++ Arrays/ArrayCC.java
@@ -4,7 +4,9 @@ public class ArrayCC {
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10,12,14,16};
         int key = 15;
-        System.out.print(linearSearch(numbers,key));
+        System.out.println(linearSearch(numbers,key));
+        System.out.println(getLargestNumber(numbers));
+        System.out.println(getSmallestNumber(numbers));
     }
     static int linearSearch(int arr[], int key){
         for(int i =0;i<arr.length;i++){
@@ -13,5 +15,25 @@ public class ArrayCC {
             }
         }
         return -1;
+    }
+
+    static int getLargestNumber(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            if(largest < arr[i]){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    static int getSmallestNumber(int arr[]){
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            if(smallest > arr[i]){
+                smallest = arr[i];
+            }
+        }
+        return smallest;
     }
 }
